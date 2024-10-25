551dba5e2716d33594636d4ae5d3c5088008b5db
hyanice <yanice.hourcade@ynov.com>
2024-10-25 12:15:48 +0200

    partie2

--- chat_client_ii_3.py
+++ chat_client_ii_3.py
@@ -7,7 +7,7 @@ host = '10.10.10.11'
 port = 13337
 
 async def send_message(writer):
-    message = await aioconsole.ainput("Entrez votre message : ")
+    message = await aioconsole.ainput("Chat : ")
     writer.write(message.encode('utf-8'))
     await writer.drain()
 
@@ -20,10 +20,16 @@ async def receive_message(reader):
         print(f"Le serveur a répondu : {repr(data.decode('utf-8'))}")
 
 async def main():
-    reader, writer = await asyncio.open_connection(host, port)
-    print(f"Connecté avec succès au serveur {host} sur le port {port}")
+    while True:
+        try:
+            reader, writer = await asyncio.open_connection(host, port)
+            print(f"Connecté avec succès au serveur {host} sur le port {port}")
 
-    await asyncio.gather(send_message(writer), receive_message(reader))
+            await asyncio.gather(send_message(writer), receive_message(reader))
+
+        except Exception as e:
+            print(f"Erreur de connexion : {e}. Réessai en 5 secondes...")
+            await asyncio.sleep(5)
 
 if __name__ == '__main__':
     asyncio.run(main())
---
+++ chat_server_ii_3.py
@@ -0,0 +1,34 @@
+import asyncio
+
+host = '10.10.10.11' 
+port = 13337 
+
+async def handle_client(reader, writer):
+    addr = writer.get_extra_info('peername')
+    print(f"Un client vient de se connecter avec l'IP {addr[0]} et le port {addr[1]}")
+
+    while True:
+        data = await reader.read(1024)
+        if not data:
+            break 
+
+        message = data.decode('utf-8')
+
+        response = f"Message received from {addr[0]}:{addr[1]} : {message}"
+        writer.write(response.encode('utf-8'))  # Envoie la réponse au client
+        await writer.drain()  # Assure que les données sont envoyées
+
+    print(f"Connexion fermée pour {addr}")
+    writer.close()  # Ferme la connexion
+
+async def main():
+    server = await asyncio.start_server(handle_client, host, port)
+
+    addr = server.sockets[0].getsockname()
+    print(f'Serveur en cours d\'exécution sur {addr[0]}:{addr[1]}')
+
+    async with server:
+        await server.serve_forever()  # Maintient le serveur en cours d'exécution
+
+if __name__ == '__main__':
+    asyncio.run(main()) 
