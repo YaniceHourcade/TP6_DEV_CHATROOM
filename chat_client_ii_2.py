493b44a0776abe27e3ebcf674691b11d086e528a
hyanice <yanice.hourcade@ynov.com>
2024-10-25 11:58:54 +0200

    partie2

--- chat_client_ii_2.py
+++ chat_client_ii_2.py
@@ -1,26 +1,29 @@
 import socket
 import sys
+import asyncio
 
 host = '10.10.10.11'
 port = 13337
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+async def send_message(writer):
+    message = await aioconsole.input("Entrez votre message : ")
+    writer.write(message.encode('utf-8'))
+    await writer.drain()
 
-try:
-    s.connect((host, port))
+async def receive_message(reader):
+    while True:
+        data = await reader.read(1024)
+        if not data:
+            print("Le serveur a fermé la connexion.")
+            break
+        print(f"Le serveur a répondu : {repr(data.decode('utf-8'))}")
+
+async def main():
+    reader, writer = await asyncio.open_connection(host, port)
     print(f"Connecté avec succès au serveur {host} sur le port {port}")
 
-    message = input("Que veux-tu envoyer au serveur : ")
+    await asyncio.gather(send_message(writer), receive_message(reader))
 
-    s.sendall(message.encode('utf-8'))
+if __name__ == '__main__':
+    asyncio.run(main())
 
-    data = s.recv(1024)
-
-    print(f"Le serveur a répondu : {repr(data)}")
-
-except Exception as e:
-    print(f"Erreur lors de la connexion au serveur : {e}")
-
-finally:
-    s.close()
-    sys.exit(0)
