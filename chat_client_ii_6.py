8497cd7d550d2b022198d82cbb3ba8a024f4f9a4
hyanice <yanice.hourcade@ynov.com>
2024-10-31 12:28:05 +0100

    log

--- chat_client_ii_6.py
+++ chat_client_ii_6.py
@@ -2,13 +2,45 @@ from csv import writer
 import sys
 import asyncio
 import aioconsole  # type: ignore
+import os
+import logging
 
 HOST = "10.10.10.11"
 PORT = 13337
 
+specific_path = r"/var/log/chat_client"
+LOG_FILE = os.path.join(specific_path, "chat_client.log")
+
+# Création du répertoire spécifique si besoin
+try:
+    os.makedirs(specific_path, exist_ok=True)
+    print(f"Le répertoire '{specific_path}' est prêt.")
+except PermissionError:
+    print(f"Permission refusée : Impossible de créer '{specific_path}'.")
+    exit(1)
+except Exception as e:
+    print(f"Une erreur est survenue : {e}")
+    exit(1)
+
+# Vérification de la possibilité de créer le fichier de log
+try:
+    with open(LOG_FILE, 'a'):
+        pass
+    print(f"Le fichier de log '{LOG_FILE}' est prêt.")
+except PermissionError:
+    print(f"Permission refusée : Impossible de créer '{LOG_FILE}'.")
+    exit(1)
+except Exception as e:
+    print(f"Une erreur est survenue : {e}")
+    exit(1)
+
+# Configuration du logging
+logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 async def send_pseudo(writer):
     input = await aioconsole.ainput("Votre Pseudo : ")
+    logging.info(f"Pseudo utilisé : {input}")
     message = "Hello|" + input
     writer.write(message.encode("utf-8"))
     await writer.drain()
@@ -17,6 +49,7 @@ async def send_pseudo(writer):
 async def send_message(writer):
     while True:
         message = await aioconsole.ainput("Chat : ")
+        logging.info(f"Message envoyer : {message}")
         writer.write(message.encode("utf-8"))
         await writer.drain()
 
@@ -26,16 +59,18 @@ async def receive_message(reader):
         while True:
             data = await reader.read(1024)
             if not data:
-                print("Le serveur a fermé la connexion.")
+                print(f"Le serveur a fermé la connexion.")
+                logging.info(f"Le serveur a fermé la connexion.")
                 break
             print(f"\nChat recu : {repr(data.decode('utf-8'))}")
     except Exception as e:
         print(f"Erreur de réception de message : {e}")
+        logging.error(f"Erreur de réception de message")
     finally:
-        print("Fermeture de la connexion...")
+        print(f"Fermeture de la connexion...")
         writer.close()
         await writer.wait_closed()
-        print("Connexion fermée. Au revoir!")
+        print(f"Connexion fermée. Au revoir!")
         sys.exit(0)
 
 
@@ -44,12 +79,14 @@ async def main():
         try:
             reader, writer = await asyncio.open_connection(HOST, PORT)
             print(f"Connecté avec succès au serveur {HOST} sur le port {PORT}")
+            logging.info(f"Connexion réussie à {HOST}:{PORT}")
 
             await send_pseudo(writer)
             # Exécutez l'envoi et la réception en parallèle
             await asyncio.gather(send_message(writer), receive_message(reader))
 
         except Exception as e:
+            logging.error(f"Erreur de connexion")
             print(f"Erreur de connexion")
             break
 
--- chat_server_ii_6.py
+++ chat_server_ii_6.py
@@ -1,14 +1,46 @@
 import asyncio
 import datetime
+import os
+import logging
 
 CLIENTS = {}
 
 HOST = "10.10.10.11"
 PORT = 13337
 
+specific_path = r"/var/log/chat_serveur"
+LOG_FILE = os.path.join(specific_path, "chat_serveur.log")
+
+# Création du répertoire spécifique si nécessaire
+try:
+    os.makedirs(specific_path, exist_ok=True)
+    print(f"Le répertoire '{specific_path}' est prêt.")
+except PermissionError:
+    print(f"Permission refusée : Impossible de créer '{specific_path}'.")
+    exit(1)
+except Exception as e:
+    print(f"Une erreur est survenue : {e}")
+    exit(1)
+
+# Création du fichier de log
+try:
+    with open(LOG_FILE, 'a'):
+        pass
+    print(f"Le fichier de log '{LOG_FILE}' est prêt.")
+except PermissionError:
+    print(f"Permission refusée : Impossible de créer '{LOG_FILE}'.")
+    exit(1)
+except Exception as e:
+    print(f"Une erreur est survenue : {e}")
+    exit(1)
+
+# Configuration du logging
+logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 async def handle_client(reader, writer):
     addr = writer.get_extra_info("peername")
+    logging.info(f"Un client vient de se connecter avec l'IP {addr[0]} et le port {addr[1]}")
     print(f"Un client vient de se connecter avec l'IP {addr[0]} et le port {addr[1]}")
 
     # Stocker le client dans CLIENTS
@@ -23,6 +55,7 @@ async def handle_client(reader, writer):
             CLIENTS[addr]["pseudo"] = pseudo  # Stocker le pseudo
             time = datetime.datetime.now().strftime("%H:%M")
             print(f"{pseudo} a rejoint la room à {time}")
+            logging.info(f"{pseudo} a rejoint la room")
 
             # Annonce à tous les autres clients
             for client_addr, client in CLIENTS.items():
@@ -40,6 +73,7 @@ async def handle_client(reader, writer):
 
             message = data.decode("utf-8")
             print(f"{pseudo} a dit : {message}")
+            logging.info(f"{pseudo} a dit : {message}")
 
             # Envoi du message à tous les autres clients
             for client_addr, client in CLIENTS.items():
@@ -54,6 +88,8 @@ async def handle_client(reader, writer):
     finally:
         pseudo = CLIENTS[addr]["pseudo"]
         print(f"Connexion fermée pour {pseudo}")
+        logging.info(f"Connexion fermée pour {pseudo}")
+        
 
         del CLIENTS[addr]
 
@@ -71,6 +107,7 @@ async def main():
     server = await asyncio.start_server(handle_client, HOST, PORT)
 
     addr = server.sockets[0].getsockname()
+    logging.info(f"Serveur démarré sur {HOST}:{PORT}")
     print(f"Serveur en cours d'exécution sur {addr[0]}:{addr[1]}")
 
     async with server:
